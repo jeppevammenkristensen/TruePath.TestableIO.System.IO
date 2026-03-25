a58af3b04d999d0d9f212041939e322932203f95
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-03-25 22:42:26 +0100

    Add FileInfo extension methods for AbsolutePath overloads
    Add extension methods to IFileInfo for CopyTo, MoveTo, and Replace operations that accept AbsolutePath parameters instead of strings. Include conditional compilation for MoveTo overwrite overload. Update version suffix to alpha-02.

---
+++ TruePath.TestableIO.System.IO/Extensions/FileInfoExtensions.cs
@@ -0,0 +1,59 @@
+﻿using TruePath;
+
+namespace System.IO.Abstractions;
+
+/// <summary>
+/// Provides extension methods for the <see cref="FileInfo"/> class to enhance file manipulation capabilities.
+/// </summary>
+/// <remarks>
+/// This static class extends the functionality of the <see cref="FileInfo"/> class by providing
+/// additional utility methods for common file operations. The methods in this class are designed
+/// to simplify file handling tasks and provide convenient shortcuts for frequently used operations.
+/// All methods are implemented as extension methods and can be called directly on any <see cref="FileInfo"/> instance.
+/// </remarks>
+public static class FileInfoExtensions
+{
+    extension(IFileInfo fileInfo)
+    {
+        /// <inheritdoc cref="FileInfo.CopyTo(string)" />
+        public IFileInfo CopyTo(AbsolutePath destFileName)
+        {
+            return fileInfo.CopyTo(destFileName.Value);
+        }
+
+        /// <inheritdoc cref="FileInfo.CopyTo(string, bool)" />
+        public IFileInfo CopyTo(AbsolutePath destFileName, bool overwrite)
+        {
+            return fileInfo.CopyTo(destFileName.Value, overwrite);
+        }
+
+        /// <inheritdoc cref="FileInfo.MoveTo(string)" />
+        public void MoveTo(AbsolutePath destFileName)
+        {
+            fileInfo.MoveTo(destFileName.Value);
+        }
+        
+#if FEATURE_FILE_MOVETO_OVERWRITE
+        /// <inheritdoc cref="FileInfo.MoveTo(string, bool)" />
+        public void MoveTo(AbsolutePath destFileName, bool overwrite)
+        {
+            fileInfo.MoveTo(destFileName.Value, overwrite);
+        }
+    #endif
+
+        /// <inheritdoc cref="FileInfo.Replace(string, string?)" />
+        public IFileInfo Replace(AbsolutePath destinationFileName,
+            AbsolutePath? destinationBackupFileName)
+        {
+            return fileInfo.Replace(destinationFileName.Value, destinationBackupFileName?.Value);
+        }
+
+        /// <inheritdoc cref="FileInfo.Replace(string, string?, bool)" />
+        public IFileInfo Replace(AbsolutePath destinationFileName,
+            AbsolutePath? destinationBackupFileName,
+            bool ignoreMetadataErrors)
+        {
+            return fileInfo.Replace(destinationFileName.Value, destinationBackupFileName?.Value, ignoreMetadataErrors);
+        }
+    }
+}
